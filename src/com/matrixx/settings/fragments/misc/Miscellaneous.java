4fa3765541a4e3b97913ec298b2997f8ae20c203
Pranav Vashi <neobuddy89@gmail.com>
2026-03-23 23:40:47 +0530

    MatrixxLab: Add three fingers swipe actions [3/3]

--- src/com/matrixx/settings/fragments/misc/Miscellaneous.java
+++ src/com/matrixx/settings/fragments/misc/Miscellaneous.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2023 The LeafOS Project
+ * Copyright (C) 2022-2026 Project Matrixx
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,15 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.matrixx.settings.fragments.misc;
-
 
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
@@ -37,21 +39,67 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     public static final String TAG = "Miscellaneous";
 
-@Override
+    private static final String KEY_THREE_FINGERS_SWIPE = "three_fingers_swipe";
+
+    // System setting key
+    private static final String THREE_FINGERS_SWIPE_ACTION = "three_fingers_swipe_action";
+
+    private ListPreference mThreeFingersSwipe;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.matrixx_settings_misc);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // Init Three Finger Swipe
+        mThreeFingersSwipe = (ListPreference) prefScreen.findPreference(KEY_THREE_FINGERS_SWIPE);
+
+        if (mThreeFingersSwipe != null) {
+            int value = Settings.System.getIntForUser(
+                    getContentResolver(),
+                    THREE_FINGERS_SWIPE_ACTION,
+                    0,
+                    UserHandle.USER_CURRENT
+            );
+
+            mThreeFingersSwipe.setValue(String.valueOf(value));
+            mThreeFingersSwipe.setSummary(mThreeFingersSwipe.getEntry());
+            mThreeFingersSwipe.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mThreeFingersSwipe) {
+            String value = (String) newValue;
+            int intValue = Integer.parseInt(value);
+
+            int index = mThreeFingersSwipe.findIndexOfValue(value);
+            mThreeFingersSwipe.setSummary(mThreeFingersSwipe.getEntries()[index]);
+
+            Settings.System.putIntForUser(
+                    getContentResolver(),
+                    THREE_FINGERS_SWIPE_ACTION,
+                    intValue,
+                    UserHandle.USER_CURRENT
+            );
+            return true;
+        }
         return false;
     }
 
     public static void reset(Context context) {
         ContentResolver resolver = context.getContentResolver();
 
+        Settings.System.putIntForUser(
+                resolver,
+                THREE_FINGERS_SWIPE_ACTION,
+                0,
+                UserHandle.USER_CURRENT
+        );
     }
 
     @Override
